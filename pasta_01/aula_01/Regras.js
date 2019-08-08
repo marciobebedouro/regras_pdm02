1a06f3b3aee95cc3386e5fdd421e63655672e510
MARCIO ROGERIO MACHADO <marcio.rmachado@senacsp.edu.br>
2019-08-08 20:38:07 -0300

    Conhecendo Regras

---
+++ pasta_01/aula_01/Regras.js
@@ -0,0 +1,30 @@
+// Exemplo para não se fazer
+//let 1nome = '';
+
+//Exemplo para se fazer
+//String pode usar aspas simples ou duplas
+let nome= 'marcio';
+
+//Tipo inteiro
+let idade = 20
+
+//Tipo decimal
+let peso = 55.25
+
+//Tipo boolean
+let visualizado = true;
+
+//Criar constante
+const nome2 = 'marcio';
+
+//Exibir informações
+console.log('Este é o console.log');
+
+console.log('Exibindo variaveis');
+console.log('Nome: ' + nome);
+
+console.log('Nome: ' + nome);
+
+console.log('Nome const:' + nome2);
+console.log('Nome: ' + nome + ' idade: '+ idade);
+console.log(`Nome: ${nome}  idade ${idade}`);
