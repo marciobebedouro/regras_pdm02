57fb5f13261e9a9a2e07c02192f2b7ffe0251065
MARCIO ROGERIO MACHADO <marcio.rmachado@senacsp.edu.br>
2019-08-09 22:19:25 -0300

    Operadores Lógicos

---
+++ pasta_01/aula_01/Util.js
@@ -0,0 +1,29 @@
+//Repetir
+const abc = 'abc';
+console.log(abc.repeat(2));
+
+//Replace
+let frase = 'Hoje o dia está bonito!';
+console.log('Utilizando Replace');
+console.log(frase.replace('dia', 'noite'));
+
+//Caracter no final da frase
+
+console.log('Utilizando endsWhit');
+console.log(frase.endsWith('o'));
+
+//Caractér no inicio da frase
+
+console.log('Utilizando starsWith');
+console.log(frase.startsWith('o'));
+
+//Convertendo para minúsculo
+
+let fraseM = 'HOJE O DIA ESTÁ BONITO';
+console.log('Utilizando ToLowerCase')
+console.log(fraseM.toLowerCase());
+
+//Convertendo para maiúsculo
+console.log('Utilizando ToUpperCase');
+console.log(frase.toUpperCase());
+
